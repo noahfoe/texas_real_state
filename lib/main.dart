dc487d39a0d78340f92dcff9c9957f2bbd01972c
noahfoe <noahfoley6@gmail.com>
2021-03-30 16:13:05 -0500

    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:real_texas_state/pages/introPage/introPage.dart';
-import 'package:real_texas_state/pages/loginPage/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
