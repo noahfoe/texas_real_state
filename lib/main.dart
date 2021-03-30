a65c8cf7f0461232c3733d2d45d0ee763f71d9da
noahfoe <noahfoley6@gmail.com>
2021-03-30 16:12:05 -0500

    added basic introduction page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:real_texas_state/pages/introPage/introPage.dart';
 import 'package:real_texas_state/pages/loginPage/login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(ProviderScope(child: MyLoginPage()));
+  runApp(ProviderScope(child: IntroPage()));
 }
--- lib/pages/introPage/introPage.dart
+++ lib/pages/introPage/introPage.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:real_texas_state/pages/loginPage/login.dart';
 
 class IntroPage extends StatelessWidget {
+  void _onIntroEnd(context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (_) => new MyLoginPage(),
+      ),
+    );
+  }
+
   List<PageViewModel> getPages() {
     return [
       PageViewModel(
-        image: Image.network('https://tinyurl.com/ygecojn8'),
+        title: "Texas Real State",
+        image: Image.network(
+            'https://clipart.info/images/ccovers/1495751366Yellow-House-PNG-Clip-Art.png',
+            scale: 2),
         body: 'Real Estate Sold Here',
         footer: Text('@Texas Real State'),
       ),
       PageViewModel(
-        image: Image.network('https://tinyurl.com/yg63qlpj'),
-        body: 'Find Apartments, Condos, and Houses Here!',
+        title: "Easy way to find your new apartment",
+        image: Image.network(
+          'https://www.pinclipart.com/picdir/big/8-88292_apartment-complex-clipart-apartment-house-apartment-building-clipart.png',
+          scale: 2,
+        ),
+        body: 'Find Apartments near Texas State University!',
         footer: Text('@Texas Real State'),
       ),
       PageViewModel(
-        image: Image.network('https://tinyurl.com/yg7o9uwu'),
-        body: 'Location Services will be Used',
+        title: "We will ask to access location!",
+        image: Image.network(
+          'https://lh3.googleusercontent.com/proxy/JkerRgRjGCeT7K4PDzoxBYu1TjNAbo44vgnGxy5N90kyV6afXv85kIMwMtjP5IXkfLMFdHK7RqtrjwU2i26K83DJU9aaXTHyqlPoWKT6p8eUvXCbtRe1CDWN_locCQ',
+          scale: 2,
+        ),
+        body: 'We use Google Maps and will need your location services enabled',
         footer: Text('@Texas Real State'),
       ),
     ];
@@ -28,16 +48,22 @@ class IntroPage extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: IntroductionScreen(
-          done: Text(
-            "Done",
-            style: TextStyle(
-              color: Colors.black,
+        body: Builder(
+          builder: (context) => Center(
+            child: IntroductionScreen(
+              showNextButton: false,
+              showSkipButton: false,
+              done: Text(
+                "Done",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              onDone: () => _onIntroEnd(context),
+              pages: getPages(),
+              globalBackgroundColor: Colors.white,
             ),
           ),
-          onDone: () {},
-          pages: getPages(),
-          globalBackgroundColor: Colors.white,
         ),
       ),
     );
