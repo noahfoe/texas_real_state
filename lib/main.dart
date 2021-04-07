4c4b57bd0881206ace3688cd839c2d2459e3e1ba
noahfoe <noahfoley6@gmail.com>
2021-04-07 10:07:07 -0500

    added uml class diagram

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:real_texas_state/pages/introPage/introPage.dart';
+import 'package:dcdg/dcdg.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
